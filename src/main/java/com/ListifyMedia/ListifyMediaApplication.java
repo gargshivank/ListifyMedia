9fe34a18a7161f68f4c4c6d74b9ff10a65a4d698
Shivank Garg <shivankgarg24@gmail.com>
2024-01-05 01:11:44 +0530

    added project structure

---
+++ src/main/java/com/ListifyMedia/ListifyMediaApplication.java
@@ -0,0 +1,13 @@
+package com.ListifyMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListifyMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListifyMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ListifyMedia/ListifyMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ListifyMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListifyMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
